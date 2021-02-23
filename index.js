69b4cabe224b1e9b6f1b1572506e2076b319db83
Your Name <you@example.com>
2021-02-23 17:05:56 +0300

    adding-js-script-for-data-collection

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const state = {gender: null,
 birthDay: null,
 name: null,
 selectedNamesPool:[]};
+const nameDisplay = document.getElementById("nameDisplay");
+const setGender = () => {
+  state.gender = document.getElementById("gender").value;
+  state.selectedNamesPool = state.gender === "male" ? maleNames : femaleNames;
+  console.log(state.gender);
+  console.log(state.selectedNamesPool);
+};
+const setBirthDate = () => {
+  const date = new Date(document.getElementById("setdate").value);
+  state.birthDate = date;
+  state.birthDay = date.getDay();
+  console.log(state);
+};
