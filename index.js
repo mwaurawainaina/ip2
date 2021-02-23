cbf346f818ce687283cb33654606225e62c1aee3
Your Name <you@example.com>
2021-02-23 17:03:04 +0300

    adding-js-script-data

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const maleNames = ['kwaski', 'kwadwo', 'kwabena', 'kwaku', 'yaw', 'kofi' ];
 const femaleNames = ['Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 const state = {gender: null,
+    birthDate: null,
+birthDay: null,
+name: null,
+selectedNamesPool:[]};
