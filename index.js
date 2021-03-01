55bfb4ad0fdeea834fd2560616ca60e85797cbd5
Your Name <you@example.com>
2021-03-01 07:32:46 +0300

    adding invalid date logic to js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];  
-let femaleNames = ["Akousa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];  
+const femaleNames = ["Akousa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let century = null
+let year = null
+let month = null
+let day = null
+if(day <=0){
+  console.log('date is invalid')
+};
+if(day > 31){
+  console.log('date is invalid')
+}
