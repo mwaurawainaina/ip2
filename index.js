2276e7b17fc5bea506b7fb346e491a6b22698d5b
Your Name <you@example.com>
2021-03-05 10:49:28 +0300

    edit index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];  
-const femaleNames = ["Akousa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let century = null
-let year = null
-let month = null
-let day = null
-if(day <=0){
-  console.log('date is invalid')
-};
-if(day > 31){
-  console.log('date is invalid')
-};
-if (month <=0){console.log('month is invalid')};
-if (month >12){console.log('month is invalid')};
+const maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+const femalNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+const birthDay = document.getElementById('setbirthday').value
+const date = new Date(birthDay)
+const CC = date.getFullYear()
+const gender = document.getElementById('gender').value
+const YY = document.getElementById('setbirthday').value
+const MM = date.getMonth()
+const DD = date.getDay()
+const yy = YY.slice(8,9)
+function he ()
+   {
+    if (date<1) {alert (invalid)}
+    else if(date>31){alert(invalid)}
+   else if(MM>12){alert(invalid)}
+    else if(MM<1){alert(invalid)}
+    else{console.log('null')}}
+he()
+
+document.getElementById("generatorForm").addEventListener(
+    "submit",
+    (generateName = (e) => {
+      e.preventDefault();
+      const getAnswer = ( ( ( (CC/4) -2*CC-1) + ((5*yy/4) ) + ((26*(MM+1)/10)) + DD ) ) 
+    Math.round('getAnswer');
+    function() {if }
